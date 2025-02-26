9fd870449de973edfcb8ad631b4c358c1f9602ca
otaviovasc-inteli <otavio.vasconcelos@sou.inteli.edu.br>
2025-02-26 12:35:51 -0300

    remove seed

--- school-api-gps/bin/render-build.sh
+++ school-api-gps/bin/render-build.sh
@@ -5,4 +5,4 @@ bundle install
 
 # Rebuild the database on deploy (acceptable since you're using fake data)
 RAILS_ENV=production bundle exec rails db:migrate
-RAILS_ENV=production bundle exec rails db:seed
+# RAILS_ENV=production bundle exec rails db:seed
